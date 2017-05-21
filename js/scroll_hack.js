87d0ce90bc45ef262a8ffaae0853013a5d4b1942
Xiaoguang Chen <xgchen@amazon.com>
2017-05-21 15:55:34 +0800

    A trick to enable the game on browsers don't support swiping
    How to play: tap the outmost cells to move, i.e. if we index
    the cells from left to right and then top-down, assume that
    the indexing is zero-based, then
    * tap cell1/cell2 to move up,
    * tap cell4/cell8 to move left,
    * tap cell7/cell11 to move right,
    * tap cell13/cell14 to move down

--- js/scroll_hack.js
+++ js/scroll_hack.js
@@ -16,3 +16,24 @@ function handleScroll(direction, x, y) {
     }
     return true;
 }
+
+document.onmouseup=function (e) {
+    var rows = document.getElementsByClassName('grid-row').length
+    var gameContainer =  document.getElementsByClassName('game-container')[0]
+    var cellSide = gameContainer.offsetWidth/rows
+    var x = e.clientX - gameContainer.offsetLeft
+    var y = e.clientY - gameContainer.offsetTop
+    if (x>cellSide && x<(gameContainer.offsetWidth-cellSide)) {
+        if (y>0 && y<cellSide) {
+            handleScroll('up', x, y)
+        } else if (y>(gameContainer.offsetHeight-cellSide) && y<gameContainer.offsetHeight) {
+            handleScroll('down', x, y)
+        }
+    } else {
+        if (x>0 && x<cellSide) {
+            handleScroll('left', x, y)
+        } else if (x>(gameContainer.offsetWidth-cellSide) && x<gameContainer.offsetWidth) {
+            handleScroll('right', x, y)
+        }
+    }
+}
